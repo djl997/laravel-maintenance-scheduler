ca9024e987fbea339e96429d2f64f68adbd58fd9
Dirk Jan <dirkjan@q2-software.nl>
2022-11-15 09:59:10 +0100

    Added: translation release at past due

--- lang/de/messages.php
+++ lang/de/messages.php
@@ -4,6 +4,7 @@ return [
     'day_format' => 'l, j. F',
     'time_format' => 'H:i',
     'release_at' => ':Day um :hour sind Wartungsarbeiten für :app geplant. Das Update dauert voraussichtlich :minutes.',
+    'release_at_past_due' => 'Fur :app sind derzeit Wartungsarbeiten geplant. Wir sind voraussichtlich ab :minutes wieder verfügbar.',
     'releasing_now' => ':App  wird derzeit gewartet. Wir sind voraussichtlich ab :minutes wieder verfügbar.',
     'release_taking_longer_than_scheduled' => 'Die Wartung von :app dauert länger als erwartet. Bitte versuchen Sie es nach :minutes erneut.',
 ];
--- lang/nl/messages.php
+++ lang/nl/messages.php
@@ -4,6 +4,7 @@ return [
     'day_format' => 'l j F Y',
     'time_format' => 'H:i',
     'release_at' => ':Day om :hour is er onderhoud gepland voor :app. De update zal naar verwachting :minutes duren.',
+    'release_at_past_due' => 'Er is op dit moment onderhoud gepland voor :app. We verwachten over :minutes weer bereikbaar te zijn.',
     'releasing_now' => 'Er wordt momenteel onderhoud gepleegd aan :app. We verwachten over :minutes weer bereikbaar te zijn.',
     'release_taking_longer_than_scheduled' => 'Het onderhoud aan :app duurt langer dan verwacht. Probeer het na :minutes nog eens.',
 ];
