80df48ae2b6dd0c37eef45a3745e43c5bc8f3118
djl997 <27086857+djl997@users.noreply.github.com>
2023-02-08 16:36:53 +0100

    Create RecalculateVersionsCommand.php

---
+++ src/Console/Commands/RecalculateVersionsCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Djl997\LaravelReleaseScheduler\Console\Commands;
+
+use Djl997\LaravelReleaseScheduler\Models\ReleaseSchedule;
+use Illuminate\Console\Command;
+
+class RecalculateVersionsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'releases:recalculate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Recalculate all version numbers.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        ReleaseSchedule::recalculateVersions();
+
+        return Command::SUCCESS;
+    }
+}
